6d4bb5a5845ec5c220091c1e25b496d73126a726
San Gillis <gillis.san@gmail.com>
2014-05-20 20:57:26 +0200

    First Haskell OpenGL program

---
+++ Cube.hs
@@ -0,0 +1,23 @@
+module Cube where
+
+import Graphics.UI.GLUT
+
+vertex3f :: (GLfloat, GLfloat, GLfloat) -> IO ()
+vertex3f (x, y, z) = vertex $ Vertex3 x y z
+
+cube :: GLfloat -> IO ()
+cube w = renderPrimitive Quads $ mapM_ vertex3f
+    [ ( w, w, w), ( w, w,-w), ( w,-w,-w), ( w,-w, w),
+      ( w, w, w), ( w, w,-w), (-w, w,-w), (-w, w, w),
+      ( w, w, w), ( w,-w, w), (-w,-w, w), (-w, w, w),
+      (-w, w, w), (-w, w,-w), (-w,-w,-w), (-w,-w, w),
+      ( w,-w, w), ( w,-w,-w), (-w,-w,-w), (-w,-w, w),
+      ( w, w,-w), ( w,-w,-w), (-w,-w,-w), (-w, w,-w) ]
+
+cubeFrame w = renderPrimitive Lines $ mapM_ vertex3f
+    [ ( w,-w, w), ( w, w, w),  ( w, w, w), (-w, w, w),
+    (-w, w, w), (-w,-w, w),  (-w,-w, w), ( w,-w, w),
+    ( w,-w, w), ( w,-w,-w),  ( w, w, w), ( w, w,-w),
+    (-w, w, w), (-w, w,-w),  (-w,-w, w), (-w,-w,-w),
+    ( w,-w,-w), ( w, w,-w),  ( w, w,-w), (-w, w,-w),
+    (-w, w,-w), (-w,-w,-w),  (-w,-w,-w), ( w,-w,-w) ]
